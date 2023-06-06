b48bfdc3dac040d34d498cc9b54364ffae7c0304
venusgalstar <venusgalstar@gmail.com>
2023-06-06 16:26:25 +0900

    update value

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
-const PR_K = "7361d4e929c538a9c1e24671a0fe039b5efa4e648e74e78d65b53a8c6959bed7";
-const TOKEN_ADDRESS = '0x8a810ea8B121d08342E9e7696f4a9915cBE494B7'; // USDT address on Ethereum Goerli
-const AMOUNT = 5;
+const PR_K = "";
+const TOKEN_ADDRESS = '0xb17d901469b9208b17d916112988a3fed19b5ca1'; // plsx address on PULSECHAIN Goerli
+const AMOUNT = 250000;
 const LEVEL = 3;
 const LEVEL_DECIMAL = 1000000;
 const EXPLORER_API = "https://api.etherscan.com/api?module=contract&action=getabi&address=";
